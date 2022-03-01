bef5374ac06d9826158094b91727e071fbac39d9
Anvi Alex <epononanvialex@gmail.com>
2022-03-01 14:19:58 +0200

    Add Alembic and generate data tables in Companies database

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app import models, database
 from . import utils, database, models
 
 
-models.Base.metadata.create_all(bind=database.engine)
+# models.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
 
 
@@ -17,28 +17,22 @@ file = 'test'
 
 # utils.normalization(file)
 
-try:
-  conn = psycopg2.connect(host='localhost', database='canari', user='postgres', port='5433', password='root12', cursor_factory=RealDictCursor)
-  cursor = conn.cursor()
-  print("Connection to DB sucessfull ...")
-except Exception as error:
-  print("Connecting to database.")
-  print("Error:", error)
-
 @app.get("/")
 async def root():
     return {"message": "Hello to Canari"}
-  
-@app.get("/search")
-async def search(stack: str, city: str, db: Session = Depends(database.get_db)):
-  # companies = db.query(models.Company).filter(and_(models.Company.stack.contains(stack), models.Company.city==city)).all()
-  number = db.query(models.Company).filter(and_(models.Company.stack.contains(stack), models.Company.city==city)).count()
-  total = db.query(models.Company).count()
-  percentage = (number * 100)/total
-
-  return {"percent": f'{percentage}%', "match number": number, "total Comp": total }
 
 @app.get("/testy")
 def comp(db: Session = Depends(database.get_db)):
 
   return {"DB": "Db working"}
+  
+@app.get("/search")
+async def search(stack: str, city: str, db: Session = Depends(database.get_db)):
+  #companies = db.query(models.Company).filter(and_(models.Company.stack.contains(stack), models.Company.city==city)).all()
+  number = db.query(models.Company).filter(and_(models.Company.stack.contains(stack), models.Company.city.contains(city))).count()
+  total = db.query(models.Company).count()
+  total_companies_city = db.query(models.Company).filter(models.Company.city.contains(city)).count()
+  percentage = (number * 100)/total_companies_city
+
+  return {"percent": f'{percentage}%', "match number": number, f"total Companies in {city}": total_companies_city, "total companies": total }
+
