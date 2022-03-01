d80d748c0119cd3a71768e7107e9b9b58eacc801
Anvi Alex <epononanvialex@gmail.com>
2022-03-01 14:22:53 +0200

    Add CORS

--- app/main.py
+++ app/main.py
@@ -7,10 +7,20 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 from app import models, database
 from . import utils, database, models
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # models.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins= origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 
 file = 'test'
